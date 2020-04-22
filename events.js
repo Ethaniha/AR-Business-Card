977bfb52fa7fa37231d1ef797b8077a940a739d5
Ethan Richardson <ethaniha@gmail.com>
2020-04-22 23:15:51 +0100

    Added JS for all icons

--- events.js
+++ events.js
@@ -1,12 +1,29 @@
 AFRAME.registerComponent('markerhandler', {
 
     init: function () {
-        //const barcodeMarker = document.querySelector("#barcode-marker");
-        const aEntity = document.querySelector("#button-linkedin");
 
-        // every click, we make our model grow in size :)
-        aEntity.addEventListener('click', function (ev, target) {
+        const linkedin = document.querySelector("#button-linkedin");
+        const facebook = document.querySelector("#button-facebook");
+        const instagram = document.querySelector("#button-instagram");
+        const twitter = document.querySelector("#button-twitter");
+
+        linkedin.addEventListener('click', function (ev, target) {
             window.open("https://www.linkedin.com/in/filippo-perini-525553aa/");
+        }
+        );
+
+        facebook.addEventListener('click', function (ev, target) {
+            window.open("https://www.facebook.com/Lamborghini/");
+        }
+        );
+
+        instagram.addEventListener('click', function (ev, target) {
+            window.open("https://www.instagram.com/lamborghini/");
+        }
+        );
+
+        twitter.addEventListener('click', function (ev, target) {
+            window.open("https://twitter.com/Lamborghini/");
         }
         );
     }
